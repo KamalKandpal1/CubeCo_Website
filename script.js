eac0c2186c48dcf325bdaa8c8df3158f6be19fd2
KamalKandpal1 <101231985+KamalKandpal1@users.noreply.github.com>
2022-06-16 23:32:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Scroll to reveal HTML elements
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+
+}
+
+// Specifically for the JQuery Owl Carousel 
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
